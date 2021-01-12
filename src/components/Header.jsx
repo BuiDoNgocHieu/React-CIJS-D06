f83aa62525702c0d1debe49955a709f044af2363
ngoc hieu <ngochieu839861@gmail.com>
2021-01-13 02:15:36 +0700

    finish ultis

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../assets/css/Header.css'
+
+
+export function Header(props){
+    return (
+        <div className="header-area">
+            <h1 className="red"> ❤️🌹 Search for your favorite emoji ❤️🌹</h1>
+        </div>
+    )
+}
