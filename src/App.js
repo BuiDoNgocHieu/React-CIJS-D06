5d887eda896b9c4c8f574b6438c49a569233819a
ngoc hieu <ngochieu839861@gmail.com>
2021-01-16 12:13:27 +0700

    finish ultis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Component } from 'react'
+import { Collection } from './Components/Collection'
+import { SectionList } from './Components/SectionList'
+import { pokemon } from './pokemondata'
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokemons: pokemon,
+      remain: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      fire: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      water: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      grass: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    }
+    this.move = this.move.bind(this)
+  }
+  move(from, to, index) {
+    console.log(from, to, index)
+    this.setState((oldState) => {
+      let tempState = JSON.parse(JSON.stringify(oldState))
+      if (tempState[from][index] === 1) {
+        tempState[from][index] = 0
+        tempState[to][index] = 1
+        return tempState
+      }
+    })
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  render() {
+    return (
+      <div className="app-cont">
+        <Collection pokemons={pokemon} type="remain" remain={this.state.remain} move={this.move} />
+        <SectionList pokemons={pokemon} fire={this.state.fire} water={this.state.water} grass={this.state.grass} move={this.move} />
+      </div>
+    )
+  }
 }
-
 export default App;
---
+++ src/Components/Section.jsx
@@ -0,0 +1,39 @@
+import '../assets/css/Section.css'
+import { Pokemon } from './Pokemon'
+export function Section(props) {
+    return (
+        <div className="section-cont">
+            <div className={` type-cont ${props.type}`}>
+                <p>{props.type || 'Fire'}</p>
+            </div>
+            <div className="pokemons-cont"
+                onDrop={(e) => {
+                    console.log('this is on drop')
+                    props.move(e.dataTransfer.getData('from'), props.type, e.dataTransfer.getData('indx'))
+                }}
+                onDragOver={(e) => {
+                    e.preventDefault()
+                    // console.log('over')
+                }}
+                >
+                {
+                    // if value is zero (the pokemon i-th not in the list)
+                    // return -1 , otherwise return it's index
+                    props[props.type].map((val, indx) => {
+                        if (val != 0) return indx
+                        return -1
+                    })
+                        // select only not -1 
+
+                        .filter(val => val !== -1)
+                        .map((val) => {
+                            // console.log(val)
+                            return (
+                                <Pokemon {...props.pokemons[val]} indx={val} key={val} type={props.type} />
+                            )
+                        })
+                }
+            </div>
+        </div>
+    )
+}
