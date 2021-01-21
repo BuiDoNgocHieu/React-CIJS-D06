2a876986e54c2a6d3bdb99e0166579c51399f950
ngoc hieu <ngochieu839861@gmail.com>
2021-01-22 05:26:31 +0700

    finish ultis

---
+++ src/config/config.firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAzMK7PjOdrUNBhmsAQbY1xNgeDAQTl8L8",
+    authDomain: "react-cijs-d06-5442a.firebaseapp.com",
+    projectId: "react-cijs-d06-5442a",
+    storageBucket: "react-cijs-d06-5442a.appspot.com",
+    messagingSenderId: "457296307723",
+    appId: "1:457296307723:web:2e7a2649100aad54b9c90e"
+};
