5348e6ad05da34619504a43d0a4bc8718fa17e46
Ngọc Hiếu <76440745+BuiDoNgocHieu@users.noreply.github.com>
2021-01-23 20:53:56 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { Navigation } from './components/Navigation'
+
+
 function App() {
   return (
-    <div className="App">
+    <div className="app-cont">
       <Navigation />
     </div>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,27 @@
 import '../assets/css/Navigation.css'
 
-function Navigation(props){
+function Navigation(){
     return (
-        <div>
-            this is Navigation
+        <div className="nav-cont">
+            <div className='nav-items tittle'>
+                <a href="/">Welcome to the<h2>Todo App</h2></a>
+
+            </div>
+            <div className='nav-items tools'>
+            <a href="/signin">Sign in</a>
+                
+
+            </div>
+            <div className='nav-items tools'>
+            <a href="/signup">Sign Up</a>
+                
+
+            </div>
+            <div className='nav-items tools'>
+            <a href="/about">About Us</a>
+                
+
+            </div>
         </div>
     )
 }
