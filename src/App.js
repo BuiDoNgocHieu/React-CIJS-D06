23eea193f943cc2cbe13d9e037d62d7ca01118ea
Ngọc Hiếu <76440745+BuiDoNgocHieu@users.noreply.github.com>
2021-03-18 00:04:35 +0700

    text fullstack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import { Home } from "./component/HomePage";
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  )
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+  };
+  componentDidMount() {
+    console.log("day la did mount")
+    fetch("https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.results)
+        this.setState({
+          data: data.results
+        })
+      })
+  }
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+};
+
 
 export default App;
--- src/ulitis/ulitis.js
+++
@@ -1,3 +0,0 @@
-const endpoint = 'https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple'
-fetch(endpoint)
-    .then((response) => response.json())
